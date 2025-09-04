0583b72cd568859529e9b3d36457f0af1a9120ec
liviadfsilva <liviadfsilva@gmail.com>
2025-09-03 22:54:49 -0300

    feat: create Pets table

--- vetclinic_project/pets/models.py
+++ vetclinic_project/pets/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    breed = models.CharField(max_length=50)
+    
+    class Meta:
+        db_table = "pets"
