99a1fc7b6469d4958fdc3b0ba866ca25229767db
liviadfsilva <liviadfsilva@gmail.com>
2025-09-03 23:32:58 -0300

    feat: add new columns to Pets table

--- vetclinic_project/pets/models.py
+++ vetclinic_project/pets/models.py
@@ -4,6 +4,9 @@ class Pet(models.Model):
     name = models.CharField(max_length=100)
     age = models.IntegerField()
     breed = models.CharField(max_length=50)
+    color = models.CharField(max_length=10, default="unknown")
+    sex = models.CharField(max_length=1)
+    neutered_spayed = models.CharField(max_length=1)
     
     class Meta:
         db_table = "pets"
