3a5608db57e54296436f072edbbfc953b3d5ff49
liviadfsilva <liviadfsilva@gmail.com>
2025-09-05 15:05:25 -0300

    feat: alter Vets table name; replace date_o_birth for DateField

--- vetclinic_project/owner/models.py
+++ vetclinic_project/owner/models.py
@@ -2,13 +2,15 @@ from django.db import models
 
 class Owner(models.Model):
     name = models.CharField(max_length=100)
-    date_of_birth = models.IntegerField(max_length=8)
+    date_of_birth = models.DateField()
     email = models.CharField(max_length=100, unique=True)
     phone = models.CharField(max_length=100, unique=True)
     address = models.CharField(max_length=100, unique=True)
     city = models.CharField(max_length=100)
     zip = models.CharField(max_length=50)
     state = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
     
     class Meta:
         db_table = "owners"
--- vetclinic_project/vets/models.py
+++ vetclinic_project/vets/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class Vets(models.Model):
     name = models.CharField(max_length=100)
-    date_of_birth = models.IntegerField(max_length=8)
+    date_of_birth = models.DateField()
     email = models.CharField(max_length=100, unique=True)
     phone = models.CharField(max_length=100, unique=True)
+    
+    class Meta:
+        db_table = "vets"
