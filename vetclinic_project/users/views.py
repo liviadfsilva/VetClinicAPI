1cd75d9467e6da13dc5dabcf9710de16ead4a7c7
liviadfsilva <liviadfsilva@gmail.com>
2025-09-08 16:35:54 -0300

    feat: create logout route

--- vetclinic_project/users/views.py
+++ vetclinic_project/users/views.py
@@ -3,10 +3,24 @@ from .models import User
 from .serializer import UserSerializer
 from rest_framework.permissions import IsAuthenticated
 
-from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+    
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return Response(status=status.HTTP_205_RESET_CONTENT)
+        except Exception:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
--- vetclinic_project/vetclinic_project/urls.py
+++ vetclinic_project/vetclinic_project/urls.py
@@ -1,25 +1,10 @@
-"""
-URL configuration for vetclinic_project project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework_simplejwt.views import TokenObtainPairView
+from users.views import LogoutView
 
 schema_view = get_schema_view(
    openapi.Info(
@@ -39,6 +24,7 @@ urlpatterns = [
     path('api/appointments/', include("appointments.urls")),
     path('api/users/', include("users.urls")),
     
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/auth/logout/', LogoutView.as_view(), name='logout'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui')
 ]
